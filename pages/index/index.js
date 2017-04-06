2899d75ea46bae90c78dcc1a95b1ceff776d2e62
leglars <leglars@yixishacundeMBP.gateway>
2017-04-06 19:14:42 +1000

    index page and all component

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,11 +3,10 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
     status: {
-      value: 0,
-      label: ["游客", "", ""],
+      value: 2,
+      label: ["游客", "已注册", "学员"],
     },
     userdata: {
       label: ["完成练习", "本期剩余", "学习积分"],
@@ -16,7 +15,14 @@ Page({
         completed: 0,
         available: 0,
         point: 0
-      }
+      },
+      course: 24,
+      practiceStatus: 10,
+      grade: 78,
+    },
+    introduction: {
+      title: "口语天天练",
+      content: "口语天天练是由合肥学道吉教育科技有限公司开发。为全国的托福雅思考生提高品质1对1的口语练习服务。"
     }
   },
   changeWord: function(e) {
