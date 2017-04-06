94e7068d8479abc23fa97a889d43befd7c969ec8
leglars <leglars@yixishangcundeMacBook-Pro.local>
2017-04-07 07:06:35 +1000

    Enrollment complete

--- pages/course/enrollment/enrollment.js
+++ pages/course/enrollment/enrollment.js
@@ -1,5 +1,69 @@
 var app = getApp();
 
 Page({
-    
+    data: {
+        array: ['美国', '中国', '巴西', '日本'],
+        objectArray: [
+            {
+                id: 0,
+                name: '美国'
+            },
+            {
+                id: 1,
+                name: '中国'
+            },
+            {
+                id: 2,
+                name: '巴西'
+            },
+            {
+                id: 3,
+                name: '日本'
+            }
+        ],
+        index: 0,
+        date: '2016-09-01',
+        time: '12:01',
+        buttonState: {
+            label: "报名",
+            loading: false,
+        }
+    },
+    bindPickerChange: function () {
+        console.log('picker发送选择改变，携带值为', e.detail.value);
+        this.setData({
+        index: e.detail.value
+        });
+    },
+    bindDateChange: function(e) {
+        this.setData({
+        date: e.detail.value
+        })
+    },
+    bindTimeChange: function(e) {
+        this.setData({
+        time: e.detail.value
+        })
+    },
+    bindPrompt: function(e) {
+        if(this.data.buttonState.loading) {
+            this.setData({
+                buttonState: {
+                    label: "报名",
+                    loading: false,
+                },
+            });
+        } else {
+            this.setData({
+                buttonState: {
+                    label: "报名",
+                    loading: true,
+                },
+            });
+        }
+        
+    },
+    onLoad: function() {
+
+    }
 })
