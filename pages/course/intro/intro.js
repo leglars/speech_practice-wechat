2b6ed2090918f9c2611923865c22d287434af2af
leglars <leglars@yixishangcundeMacBook-Pro.local>
2017-04-07 05:12:05 +1000

    Intro page complete

---
+++ pages/course/intro/intro.js
@@ -0,0 +1,25 @@
+var app = getApp();
+
+Page({
+    data: {
+        title: "学道吉口语天天练",
+        date: "2017-4-10",
+        author: "学道吉梁朝伟",
+        officalAccount: {
+            name: "学道吉",
+            link: ""
+        },
+        titleImage: {
+            src: "https://c1.staticflickr.com/4/3829/33440064970_bf67cbe7df_z.jpg",
+            alt: "title image"
+        },
+        content: [
+            "【大白，卖的一手好萌】一波三折，妙趣横生。悬疑和温情，搞笑与动作，即使拍成真人版也绝对是一部好题材和好故事。大白呆萌可爱一逗比，细节刻画真实有趣，放气如放屁，警察局撕扯胶带还有低电量时如同宿醉一样。最后，“I will always with you ！”绝对催泪。开年最佳动画！",
+            "它是只充气机器人,也是个医疗伴侣，能够快速扫描，检测出人体的不正常情绪或受伤，并对其治疗。大白呆萌可爱，作为机器人医生它很称职，也是小宏最好的朋友。小宏之"
+            ]
+    },
+
+    onLoad: function() {
+
+    }
+})
