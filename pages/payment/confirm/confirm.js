92b538df2be2b447ca17e0a57fae64bcaac6094e
leglars <leglars@yixishacundeMBP.gateway>
2017-04-08 23:17:20 +1000

    Add payment confirmation page

--- pages/payment/confirm/confirm.js
+++ pages/payment/confirm/confirm.js
@@ -1,5 +1,15 @@
 var app = getApp();
 
 Page({
-    
+    data: {
+        payItem: "",
+        amount: 150.00
+    },
+    bindPay: function() {
+        // upload payment detail info under user data
+        // redirect to result
+    },
+    onLoad: function() {
+        // get payment info from server
+    }
 })
